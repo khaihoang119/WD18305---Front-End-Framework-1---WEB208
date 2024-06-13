da43c9c0644bbcc8810d327bf1054c150dd91332
tranhuudat050803 <132972331+tranhuudat050803@users.noreply.github.com>
2024-06-13 08:20:54 +0700

    n

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { CreateProjectComponent } from './project/create-project/create-project.component';
@@ -9,31 +10,27 @@ import { TaskListComponent } from './task/task-list/task-list.component';
 import { EditProjectComponent } from './project/edit-project/edit-project.component';
 import { ProjectListComponent } from './project/project-list/project-list.component';
 import { EditTaskComponent } from './task/edit-task/edit-task.component';
-import { AdminComponent } from './admin/admin.component';
-import { AuthGuard } from './auth.guard';
+
+
+
 
 const routes: Routes = [
-  {
-    path: 'admin', component: AdminComponent,
-    canActivate: [AuthGuard], data: { expectedRole: 'admin' },
-    children: [
-      { path: '', redirectTo: '/admin/dashboard', pathMatch: 'full' },
-      { path: 'dashboard', component: DashboardComponent },
-      { path: 'list-project', component: ProjectListComponent },
-      { path: 'create-project', component: CreateProjectComponent },
-      { path: 'edit-project/:id', component: EditProjectComponent },
-      { path: 'create-task', component: CreateTaskComponent },
-      { path: 'list-task', component: TaskListComponent },
-      { path: 'edit-task/:id', component: EditTaskComponent }
-    ]
-  },
-  { path: 'login', component: LoginComponent },
-  { path: 'create', component: CreateComponent },
-  { path: '', redirectTo: '/admin/dashboard', pathMatch: 'full' }  // Default redirect
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'list-project', component: ProjectListComponent},
+  { path: 'create-project', component: CreateProjectComponent},
+  { path: 'edit-project/:id', component: EditProjectComponent},
+  { path: 'create-task', component: CreateTaskComponent},
+  { path: 'list-task', component: TaskListComponent},
+  { path: 'edit-task', component: EditTaskComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'create', component: CreateComponent},
+
 ];
 
 @NgModule({
   imports: [
+    CommonModule,
     RouterModule.forRoot(routes)
   ],
   exports: [RouterModule]
